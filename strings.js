22d15d7085adb72af46cedb378fb966481635151
BasmaAltayeh <basma.altayeh@gmail.com>
2019-03-20 09:50:52 +0200

    sol

--- strings.js
+++ strings.js
@@ -1,7 +1,11 @@
 // Write a function called repeatStr which repeats the given
 // string string exactly n times.
 // repeatStr("hello",4) ==> 'hellohellohellohello'
-	
+	var str=""
 	function repeatStr (n, s) {
-		//your code is here
+		if(s=0){
+			return""
+		}else{
+			return str = str +n +repearStr(n,s-1)
+		}
 	}
