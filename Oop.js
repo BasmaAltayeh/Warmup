d2524e66c198e187c10dd6f6364065ed0e907315
madinasheikhcm <muhiimcm@gmail.com>
2019-03-25 09:42:39 +0200

    sol

--- Oop.js
+++ Oop.js
@@ -1,30 +1,43 @@
 // We have this code from Adding Methods With Closures lecture: 
 // Refactor the code as an MakeGame class that shares its methods across different instances.
 
-
 function randInt(n) {
     return Math.floor(Math.random() * (n + 1));
 }
 
 function makeGame(upperbound){
-  var upper=upperbound;
-  var rand=randInt(upper)
-  var counter=0;
-  return {
-  	guessMyNumber:function(n){
-  		counter++;
-	    if (n > upper) {
-	      return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
-	    } else if (n === rand) {
-	      return "You guessed my number!";
-	    }
-	    return "Nope! That wasn't it!";
-  	},
-  	giveUp:function(){
-    	return rand;
-  	},
-  	numOfGuesses:function(){
-    	return counter;
-  	}
-  }
+  var inst ={};
+  inst.upper : upperbound;
+  inst.rand : randInt(upper);
+  inst.counter :0;
+  inst.guessMyNumber =guessMyNumber;
+  inst.giveUp=giveUp;
+  inst.numOfGuesses =numOfGuesses;
+
+  return inst;
+
 }
+
+ var guessMyNumber=function(n){
+      counter++;
+      if (n > upper) {
+        return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
+      } else if (n === rand) {
+        return "You guessed my number!";
+      }
+      return "Nope! That wasn't it!";
+    }
+
+ var giveUp =   function(){
+      return rand;
+    }
+
+ var numOfGuesses = function(n){
+      counter++;
+      if (n > upper) {
+        return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
+      } else if (n === rand) {
+        return "You guessed my number!";
+      }
+      return "Nope! That wasn't it!";
+    } 
