fb3bcf5ecb1ce068722c34f7357266b0203dbcd4
BasmaAltayeh <basma.altayeh@gmail.com>
2019-03-27 09:45:23 +0200

    solution

--- lastWarmup.js
+++ lastWarmup.js
@@ -7,4 +7,14 @@ var nums = [2, 7, 11, 15], target = 9,
 twoSum(nums,9); //should return [0,1] because nums[0] + nums[1] = 2 + 7 = 9;
 */
 
+function twoSum (array,sum){
+	for(var i=0; i < array.length ; i++){
+		
+		for (var j=0; j < array.length ; j++){}
+		
+	}
+	
+	
+}
+
 
